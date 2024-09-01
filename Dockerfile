3e95ef05defde769334e4a3dd9c1e2fc9692aab6
Subodh Sharma <subodh.sharma@arinco.com.au>
2024-09-01 19:21:34 +1000

    printing to console

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
-FROM python:3.10-slim 
+FROM python:3.9
 WORKDIR /app
 COPY . /app
-RUN pip install --no-cache-dir -r requirements.txt
+RUN python -m unittest discover -s . -p "test.py"
 CMD ["python", "Problem1/main.py", "--spec_file_path", "spec.json", "--fixed_width_file_path", "sample_fixed_width_file", "--delimiter", ",", "--delimiter_file_path", "sample_delimiter"]
-
--- Problem1/main.py
+++ Problem1/main.py
@@ -14,29 +14,43 @@ if __name__ == '__main__':
     argpass.add_argument("--delimiter_file_path", required=True, type=str)
     args = argpass.parse_args()
 
+
     # defining local vars from arguments passed
     spec_file_path = args.spec_file_path
     fixed_width_file_path = args.fixed_width_file_path
     delimiter = args.delimiter
     delimiter_file_path = args.delimiter_file_path
 
+    print("Starting generate fixed width file script......")
     # initializing generate fixed width file class
     generate_file_init = GenerateFixedWidthFile()
     # validating and reading the json spec file
+    print("Reading spec file....")
     generate_file_init.read_file(spec_file_path)
     # faking the number of rows of data
+    print("Creating content....")
     content = generate_file_init.create_content(10)
     # exporting the fixed width file
+    print("Content generated\n")
+    print(content)
+    print(f"\ncontent written to {fixed_width_file_path} filepath...")
     generate_file_init.write_file(content, fixed_width_file_path)
 
 
     # initialise the parse fixed width class
+    print("\nStarting parse fixed width file script....")
     parse_file_init = ParseFixedWidthFile()
     # reading the spec config file used to parse the file
     parse_file_init.read_config_file(spec_file_path)
     # reading fixed width file
+    print("Reading fixed width file....")
+    print("Fixed width file content....\n")
     content = parse_file_init.read_fixed_width_file(fixed_width_file_path)
+    print(content)
     # parsing the fixed width content to delimited content, providing delimiter here
+    print("\nParsed Content....")
     parsed_content = parse_file_init.parse_to_delimited(content, delimiter)
+    print(parsed_content)
     # writing delimiter content to file 
     parse_file_init.write_delimiter_file(parsed_content, delimiter_file_path)
+    print(f"\nparsed content written to {delimiter_file_path} file!!!!")
